851947efeed6da6da12dfb478489569c82c4c3f8
p1rv <karol.p.krol@gmail.com>
2023-02-24 23:17:45 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>OK, I am editing it now</p>
+        <p>But am I?</p>
+        <p>Allright it works fine</p>
         <a
           className="App-link"
           href="https://reactjs.org"
